a8cb4846b5c5da920869a471288d00ebf1084e23
vivek <vivekchaudhary160723@gmail.com>
2024-11-17 13:05:53 +0530

    3.13 second try1

--- Basic_programes/Armstrog.py
+++ Basic_programes/Armstrog.py
@@ -1,12 +1,11 @@
-n = int(input("Enter a number: "))
-num = len(str(n))
-sum = 0
-temp = n
-while temp > 0:
-    digit = temp % 10
-    sum += digit ** num
-    temp //= 10
-if sum == n:
-    print(f"{n} is an Armstrong number.")
+num = int(input("Enter the number: "))
+temp = num
+n = len(str(num))
+ans = 0
+while num > 0:
+    ans += (num % 10) ** n
+    num //= 10
+if temp == ans:
+    print("Yes, it is an Armstrong number")
 else:
-    print(f"{n} is not an Armstrong number.")
+    print("Not an Armstrong number")
