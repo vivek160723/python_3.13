acdc3dca69302aaadc98790e024bf67c4dc1e323
vivek <vivekchaudhary160723@gmail.com>
2025-02-03 12:47:37 +0530

    3.13 second try1

---
+++ Sorting/Searching/insertion.py
@@ -0,0 +1,14 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+numbers = [12, 11, 13, 5, 6]
+print("Unsorted list:", numbers)
+insertion_sort(numbers)
+print("Sorted list:", numbers)
